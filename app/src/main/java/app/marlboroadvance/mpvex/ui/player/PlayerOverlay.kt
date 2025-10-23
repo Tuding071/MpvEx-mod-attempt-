4c4971ebf6fd4e47098d9ec16c5fac6ec43b2d0e
Tuding071 <davidpenaranda071@gmail.com>
2025-10-23 13:59:51 +0800

    Add offset import to PlayerOverlay.kt

--- app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
