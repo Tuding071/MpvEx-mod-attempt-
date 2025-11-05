d7f1e2a69bf00c8dc578ac505de4240056f85003
Tuding071 <davidpenaranda071@gmail.com>
2025-11-05 11:52:31 +0800

    Update PlayerOverlay.kt

--- app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
@@ -55,6 +55,7 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.ui.input.pointer.pointerInput
 import android.content.Intent
 import android.net.Uri
+import kotlin.math.abs
 
 @Composable
 fun PlayerOverlay(
