5a7aba70b6e3d7b718952e8af55d93e4f8f274c4
Tuding071 <davidpenaranda071@gmail.com>
2025-11-05 15:47:47 +0800

    Update PlayerOverlay.kt

--- app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
@@ -420,7 +420,7 @@ fun PlayerOverlay(
             }
             currentPosition = currentPos
             videoDuration = duration
-            delay(500)
+            delay(50)
         }
     }
     
