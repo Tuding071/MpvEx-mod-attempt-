d57495d37c7caede7e0992d314c88636e6b272d1
Tuding071 <davidpenaranda071@gmail.com>
2025-11-08 17:43:33 +0800

    Update PlayerOverlay.kt

--- app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
@@ -95,7 +95,7 @@ fun PlayerOverlay(
     var videoFPS by remember { mutableStateOf(30.0) } // Default, will detect actual FPS
     
     // Frame scrubbing constants
-    val pixelsPerFrame = 6 // Sensitivity - higher = less sensitive
+    val pixelsPerFrame = 48 // Sensitivity - higher = less sensitive
     val frameDebounceMs = 33L // ~30fps command rate
     val preDecodeWindowSeconds = 3.0 // 3 seconds past and future
     
