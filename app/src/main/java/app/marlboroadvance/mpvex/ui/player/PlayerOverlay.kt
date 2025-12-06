53a5c59909bbcc37d01d3f2bae67630c223d40f7
Tuding071 <davidpenaranda071@gmail.com>
2025-12-06 16:37:28 +0800

    Update PlayerOverlay.kt

--- app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
+++ app/src/main/java/app/marlboroadvance/mpvex/ui/player/PlayerOverlay.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
